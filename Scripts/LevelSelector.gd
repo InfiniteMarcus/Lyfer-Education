e58c555a247e881635121231a3fdd5f12737a2c8
Infinitemarcus <marcus.natrielli@outlook.com>
2021-01-15 20:31:53 -0300

    Primeiro commit

---
+++ Scripts/LevelSelector.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _on_Level01_pressed():
+	get_tree().change_scene("res://Levels/Level01.tscn")
---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _on_Boto__Comear_pressed():
+	get_tree().change_scene("res://LevelSelector.tscn")
+
+
+func _on_Boto__Instrues_pressed():
+	pass # Replace with function body.
+
+
+func _on_Boto__Sair_pressed():
+	get_tree().quit()
