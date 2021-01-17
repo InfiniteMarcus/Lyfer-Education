5dd3fdfe0d0045880861168cff52ad60f518c86e
Infinitemarcus <marcus.natrielli@outlook.com>
2021-01-17 01:15:18 -0300

    feat: adicionando menus de seleção de fases

--- Scripts/CoinController.gd
+++ Scripts/CoinController.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
+var sound
+
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
+	sound = get_node("../../SoundEffects")
 	
 func _on_body_entered(other):
 	if (other.has_method("add_coin")):
 		other.add_coin()
 		queue_free()
-		var sound = get_node("../../SoundEffects")
 		sound.stream = load("res://Musics/Coin.wav")
 		sound.play()
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,7 +4,7 @@ func _ready():
 	MusicController.play_music()
 
 func _on_Boto__Comear_pressed():
-	get_tree().change_scene("res://LevelSelector.tscn")
+	get_tree().change_scene("res://Scenes/LevelSelector.tscn")
 	
 func _on_Boto__Instrues_pressed():
 	get_tree().change_scene("res://HowToPlay.tscn")
@@ -14,3 +14,12 @@ func _on_Button_pressed():
 
 func _on_Boto__Personagem_pressed():
 	get_tree().change_scene("res://CharacterSelector.tscn")
+
+func _on_Level01_pressed():
+	get_tree().change_scene("res://Scenes/LevelSelector5.tscn")
+
+func _on_Level02_pressed():
+	get_tree().change_scene("res://Levels/Level01.tscn")
+
+func _on_Level03_pressed():
+	get_tree().change_scene("res://Scenes/LevelSelector6.tscn")
